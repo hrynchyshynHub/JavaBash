82e5ed20488498c74259deda66ddd834feec24e8
Ivan Hrynchyshyn <ivan.hrynchyshyn@eleks.com>
2018-04-19 16:45:01 +0300

    Created Main Scene and Enter handler.

--- src/com/javabash/Main.java
+++ src/com/javabash/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("view/home.fxml"));
+        primaryStage.setTitle("Java Bash");
+        primaryStage.setScene(new Scene(root, 800, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
--- src/com/javabash/controller/Controller.java
+++ src/com/javabash/controller/Controller.java
@@ -1,4 +1,18 @@
 package com.javabash.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+
+
+    @FXML
+    private void execute(KeyEvent event){
+        if(event.getCode().equals(KeyCode.ENTER) || event.getCharacter().getBytes()[0] == '\n' || event.getCharacter().getBytes()[0] == '\r') {
+            // your action
+            System.out.println("Enter pressed");
+        }
+    }
 }
